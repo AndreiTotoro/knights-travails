ad3bc69fd66d1693742b7ae31f0aeea4759c5bfb
AndreiTotoro <AndreiTotoro@gmail.com>
2022-10-18 16:55:08 +0300

    Generate the nodes on the chess board

--- main.rb
+++ main.rb
@@ -1,4 +1,36 @@
+class Node
+  def initialize(value)
+    @value = value
+    @adjacent_nodes = []
+  end
+
+  def add_edge(adjacent_node)
+    @adjacent_nodes.push(adjacent_node)
+  end
+
+  attr_reader :value
+end
 # Put together a script that creates a game board and a knight.
+
+class Gameboard
+  def initialize
+    i = 1
+    letter = 65
+    64.times do
+      if i == 9
+        i = 1
+        letter += 1
+        print "\n"
+      end
+
+      new_node = Node.new("#{letter.chr}#{i}")
+      i += 1
+      print "#{new_node.value} "
+    end
+  end
+end
+
+Gameboard.new
 
 # Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
 
